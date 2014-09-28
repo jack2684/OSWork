9e43de86fe679c4ad04542eacdcbba62a0f8d9a3
Junjie Guan <gjj2684@gmail.com>
2014-09-28 12:03:50 -0400

    Pass the compile

--- src/bridge.c
+++ src/bridge.c
@@ -24,109 +24,126 @@
 
 
 pthread_mutex_t lock =  PTHREAD_ERRORCHECK_MUTEX_INITIALIZER_NP;
+int maxCar, n2h, h2n, flow = 0, car = 0;
 
+void deQueueWithinLock(int direction) {
+    if(direction > 0) {
+        n2h--;
+    } else {
+        h2n++;
+    }
+}
 
-// for pretty printing
-char *spaces(int kid) {
-  switch (kid) {
-  case 0:
-    return ":";
-  case 1:
-    return "--";
-  case 2:
-    return "++++";
-  case 3:
-    return "======";
-  }
+void exitBridge(int direction) {
+    // Lock the share resource
+    int rc = pthread_mutex_lock(&lock);
+    if(rc) {
+        fprintf(stderr, "Lock acquire fails.\n");
+        exit(-1);
+    }
 
-  return "********";
-	
-}  
+    // Checking stats
+    if(car > maxCar) {
+        fprintf(stderr, "#Cars on bridge %d overflow %d\n", car, maxCar);
+    } 
+    if(car < 0) {
+        fprintf(stderr, "#Cars on bridge %d is negative\n", car);
+    }
+    if(car < maxCar) {
+        if(flow > 0 && n2h > 0) {
+            fprintf(stderr, "#Cars on bridge %d < %d, with %d remaining cars waiting to go to Hanvoer\n", car, maxCar, n2h);
+        }
+        if(flow < 0 && h2n > 0) {
+            fprintf(stderr, "#Cars on bridge %d < %d, with %d remaining cars waiting to go to Norwich\n", car, maxCar, h2n);
+        }
+    }
 
+    // Update stats
+    car--;
 
-void *lockfun(void *vargp) {
+    // Release the lock
+    rc = pthread_mutex_unlock(&lock);
+    if(rc) {
+        fprintf(stderr, "Lock acquire fails.\n");
+        exit(-1);
+    }
+}
 
+void onBridge(int direction) {
+   sleep(1);
+}
 
-  int me = (int) vargp;
-  int rc;
+void arriveBridge(int direction) {
+    int rc, enterTheBridge = 0;
+    while(!enterTheBridge) {
+        // Lock the share resource
+        rc = pthread_mutex_lock(&lock);
+        if(rc) {
+            fprintf(stderr, "Lock acquire fails.\n");
+            exit(-1);
+        }
 
+        // Read and perceive the locked resource       
+        if(car == 0) {
+            enterTheBridge = 1;
+        } else if(car <= maxCar) {
+            if(flow == direction) {
+                enterTheBridge = 1;
+            }
+        }
 
-  printf("%s Thread %d wants to work on the complex data structure.\n", spaces(me), me);
-  printf("%s Thread %d: trying to acquire lock...\n\n",spaces(me), me);
+        // Write the locked resource if necessary
+        if(enterTheBridge) {
+            deQueueWithinLock(direction);
+            flow = direction;
+        }
 
-  rc = pthread_mutex_lock(&lock);
-  if (rc) {
-    printf("Thread %d: acquire failed!\n", me);
-    exit(-1);
-  }
-  printf("\n%s Thread %d: acquired the lock!\n\n", spaces(me), me);  
+        // Release the lock
+        rc = pthread_mutex_unlock(&lock);
+        if(rc) {
+            fprintf(stderr, "Lock acquire fails.\n");
+            exit(-1);
+        }
+    }
 
-  printf("%s Thread %d: In critical section.  Now I can work on the data structure and temporarily make the invariant false!\n", spaces(me), me);
+}
 
-  sleep(1);
-
-  printf("%s (Thread %d: pretending to work on the data)\n", spaces(me), me);
-
-  sleep(1);
-
-  printf("%s Thread %d: All done.  I've restored the invariant. About to release lock...\n", spaces(me), me);
-  rc = pthread_mutex_unlock(&lock);
-  if (rc) {
-    printf("Thread %d: release failed!\n", me);
-    exit(-1);
-  }
-
-  
-  printf("%s Thread %d: exiting.\n",spaces(me), me);
-
-  return NULL;
+void oneVehicle(int direction) {
+    arriveBridge(direction);
+    onBridge(direction);
+    exitBridge(direction);
 }
 
 
+int main(int argc, char *argv[]) {
+    int rc, i; 
+    if(argc != 4) {
+        printf("brdige usage: ./bridge <#maxCar on the bridge> <#cars to Hanover> <#car to Norwich>\n");
+        return;
+    } else {
+        maxCar = atoi(argv[1]);
+        n2h = atoi(argv[2]);
+        h2n = atoi(argv[3]);
+    }
+    pthread_t* carThreads = (pthread_t*) malloc(sizeof(pthread_t) * (n2h + h2n));
 
-
-//==================================
-
-int
-main(int argc, char *argv[]) {
-    if(argc <= 1) {
-        printf("brdige usage: ./bridge <#cars to Hanover> <#car to Norwich>")
+    // Start the children threads
+    for(i = 0; i < n2h + h2n; i++) {
+        if(i < n2h) {
+            rc = pthread_create(&carThreads[i], NULL, oneVehicle, 1);
+        } else {
+            rc = pthread_create(&carThreads[i], NULL, oneVehicle, -1);
+        }
     }
 
-
-  pthread_t kids[KIDS];
-
-  int i, rc;
-  void *(*kidfun)(void *);
-
-
-
-
-  for (i = 0; i < KIDS; i++) {
-    
-    rc = pthread_create(&kids[i],  // thread data structure to be written
-			NULL,    // attributes (we'll ignore)
-			lockfun, // the function to be run 
-                        (void *) i); // the argument to the function
-
-    if (rc) {
-      printf("pthread_create failed!\n");
-      exit(-1);
+    // Wait till the end
+    for(i = 0; i < n2h + h2n; i++) {
+        printf("Waiting for %dth child", i);
+        rc = pthread_join(carThreads[i], NULL);
     }
 
-  }
-
-
-  for (i = 0; i < KIDS; i++) {
-
-    rc = pthread_join(kids[i],  // thread to wait for
-		      NULL);   // where to save the rc from pthread_exit(), 
-                             // (if we care. for this example, we don't)
-
-  }
-
-
-  return 0;
+    printf("Simulation complete.\n");
+    return 0;
 }
   
 
